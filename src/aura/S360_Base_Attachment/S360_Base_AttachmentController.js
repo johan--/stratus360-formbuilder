e5c725d8ff58a5c1c6ce73d513b3c5f072a50c4c
tiyo.lab <tiyo.lab@gmail.com>
2018-05-14 16:27:09 -0400

    add object level security to rendered button

--- src/aura/S360_Base_Attachment/S360_Base_AttachmentController.js
+++ src/aura/S360_Base_Attachment/S360_Base_AttachmentController.js
@@ -1,6 +1,23 @@
 ({
-    handleUploadFinished: function(component, event, helper){
-        alert("WEEOOO");
+    doSave: function(component, event, helper) {
+        
+        if (component.find("fileId").get("v.files").length > 0) {
+            helper.uploadHelper(component, event);
+        } else {
+            alert('Please Select a Valid File');
+        }
+    },
+ 
+    handleFilesChange: function(component, event, helper) {
+        /*var fileList = [];
+        len = event.getSource().get("v.files").length;
+        alert("length is "+len);
+       
+        for(var i = 0; i<len; i++){
+            fileList.push(event.getSource().get("v.files")[i]);
+        }
+        component.*/
+        component.set("v.FileList",event.getSource().get("v.files"));
     },
     showDiv : function(component, event, helper) {
         document.getElementById('uploadDiv').style.display = 'block';
