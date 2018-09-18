365ffce19238e760639a77ead8b6a1bd9284552e
Samiur <samiur.rahman@stratus360.com>
2018-09-18 14:04:55 -0400

    update

--- ngFormBuilder/gulpfile.js
+++ ngFormBuilder/gulpfile.js
@@ -1,8 +1,6 @@
 'use strict';
 
 var gulp = require('gulp');
-var pump = require('pump');
-var uglify = require('gulp-uglify');
 //var gutil = require('gulp-util');
 var plugins = require('gulp-load-plugins')();
 plugins.source = require('vinyl-source-stream');
@@ -32,7 +30,3 @@ gulp.task('build', function(cb) {
 });
 gulp.task('watch', require('./gulp/watch')(gulp, plugins));
 gulp.task('default', ['build', 'watch']);
-
-gulp.task('uglify-debug', function(callback) {
-  pump([gulp.src('./src/**/*.js'), uglify(), gulp.dest('./dist/')], callback);
-});
